4a4cd82ec856bed576b4c10b1d08d29d2b666d2b
leresi <kevinleparwa@gmail.com>
2021-06-25 13:21:22 +0300

    handled user click

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#sendButton").click(function(){
+        var name = $("#nameInput").val();
+        var email =$("#emailInput").val();
+        var message =$("#messageInput").val();
+        const userResponse={name: name, email:email, message:message}
+        // alert(userResponse);
+        $.ajax({
+			type: "POST",
+		url: "https://bigbreakbjj.us14.list-manage.com/subscribe",
+			dataType: "jsonp",
+			data: {
+                userResponse
+			},
+			success: function(data) {
+				console.log(data);
+				alert("Subscribed!");
+			},
+			error: function() {
+                alert(`thank you ${name} for reaching out to us`)
+			}
+		});
+    });
+
+});
